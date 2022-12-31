69d472202ebc73d992bcaabea690d3df70890030
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-31 06:03:32 +0200

    make props required

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -26,7 +26,7 @@ ContactList.propTypes = {
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
-    })
+    }).isRequired
   ),
   deleteContact: PropTypes.func.isRequired,
 };
